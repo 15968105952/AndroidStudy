52df72b39b69cab890685542136fbbb94e06a8ce
mazhuang2020 <9995108575@qq.com>
2020-05-05 23:21:32 +0800

    反射添加

--- app/src/main/java/com/lahm/learndaemon/activity/reflex/Person.java
+++ app/src/main/java/com/lahm/learndaemon/activity/reflex/Person.java
@@ -3,8 +3,16 @@ package com.lahm.learndaemon.activity.reflex;
 import android.util.Log;
 
 public class Person {
-    public String say(String what){
-        Log.i(Person.class.getSimpleName(),what);
-        return  what;
+    public int getAge() {
+        return age;
+    }
+
+    private int age;
+    private String name;
+    private String sex;
+
+    private String say(String what) {
+        Log.i(Person.class.getSimpleName(), what);
+        return what;
     }
 }
--- app/src/main/java/com/lahm/learndaemon/activity/reflex/ReflexActivity.java
+++ app/src/main/java/com/lahm/learndaemon/activity/reflex/ReflexActivity.java
@@ -3,17 +3,21 @@ package com.lahm.learndaemon.activity.reflex;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
 import com.lahm.learndaemon.R;
 import com.lahm.learndaemon.activity.BaseActivity;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ReflexActivity extends BaseActivity {
 
     private MediaPlayer mediaPlayer;
+    private String TAG = ReflexActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +27,38 @@ public class ReflexActivity extends BaseActivity {
     }
 
     public void reflex(View view) {
+        getPersonMethod();
+        getAndSetAge();
+        //未成功，show方法无法反射调用
+        showToast();
+
+    }
+
+    private void showToast() {
+        View inflate = LayoutInflater.from(ReflexActivity.this).inflate(R.layout.ui_toast, null);
+        MyToast myToast = new MyToast(ReflexActivity.this, inflate);
+        myToast.show();
+    }
+
+    private void getAndSetAge() {
+        Person person = new Person();
+        //获取字节码
+        Class<? extends Person> aClass = person.getClass();
+        //2.获取私有属性
+        try {
+            Field age = aClass.getDeclaredField("age");
+            age.setAccessible(true);
+            Log.i(TAG, "age:" + age);
+            Toast.makeText(ReflexActivity.this, age.toString(), Toast.LENGTH_SHORT).show();
+            //3.改变私有属性的值
+            age.set(person, 18);
+            Log.i(TAG, "age:" + person.getAge());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getPersonMethod() {
         Person person = new Person();
         //反射步骤
         try {
@@ -30,13 +66,15 @@ public class ReflexActivity extends BaseActivity {
             Class aClass = Class.forName("com.lahm.learndaemon.activity.reflex.Person");
 //            Class<? extends Person> aClass = person.getClass();
             //2.获得方法
-            Method method = aClass.getMethod("say", String.class);
+            //获得私有方法 aClass.getDeclaredMethod
+            //获得共有方法 aClass.getMethod
+            Method method = aClass.getDeclaredMethod("say", String.class);
+            method.setAccessible(true);
             //调用方法
             String data = (String) method.invoke(person, "测试反射");
-            Toast.makeText(ReflexActivity.this,data,Toast.LENGTH_SHORT).show();
+            Toast.makeText(ReflexActivity.this, data, Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
